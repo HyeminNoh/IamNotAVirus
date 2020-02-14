d3191e43d4b24c1091a62e0dbe582ff06fe9c1df
HyeminNoh <shrmffkfk@naver.com>
2020-02-15 03:18:04 +0900

    메인 컨텐츠 내용 수정

--- vue/src/views/Main.vue
+++ vue/src/views/Main.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-        <b-container>
+    <div id="Content">
+        <b-container id="mainContent">
             <b-row>
-                <b-col cols="6">
+                <b-col>
                     <b-embed
                             type="iframe"
                             aspect="16by9"
@@ -12,20 +12,38 @@
                 </b-col>
                 <b-col>
                     <div class="text-left">
-                        <h4>#iamnotavirus운동에 대해 아시나요?</h4>
-                        <p>
-                            신종 코로나 바이러스가 전세계에 퍼져나가면서 많은 사람들이 두려움을 느끼고 있습니다.<br>
-                            이런 두려움과 별개로 신종 코로나 바이러스가 인종 차별의 요소로 사용되고 있습니다.<br><br>
-                            모든 아시안들이 코로나를 보균했을거라는 생각은 인종 차별적인 생각입니다.<br>
-                            신종 코로나에 대해 올바르게 인식하고 모든 아시아인들에 대한 차별을 멈춰주세요.<br>
+                        <h5 style="font-weight: bold;">#IamNotAVirus 운동에 대해 아시나요?</h5>
+                        <p class=mainDetail>
+                            코로나19가 전세계에 퍼져나가면서 많은 사람들이 두려움을 느끼고 있습니다.
+                            일부에서는 모든 동양인을 보균자 취급하며 차별하는 현상이 일어나고 있습니다.
+                            이런 현상들은 인종차별적인 행동입니다.
+                        </p>
+                        <p class=mainDetail style="font-weight: bold">
+                            코로나19를 인종차별을 위한 요소로 사용하지말아주세요.<br>
+                            코로나19에 대해 올바르게 인식하고 차별을 멈춰주세요.
                         </p>
                     </div>
-                    <div class="text-right">
-                        <b-button variant="primary" href="#">코로나-19 알아보기</b-button>
-                        <b-button variant="success" href="#">전세계 현황</b-button>
+                    <div class="text-right" style="margin-top: 50px;">
+                        <b-button variant="primary" class="MoreBtn" to="/covid19">코로나19 알아보기</b-button>
+                        <b-button variant="success" class="MoreBtn" to="infection">전세계 현황</b-button>
                     </div>
                 </b-col>
             </b-row>
         </b-container>
     </div>
 </template>
+<style lang="css">
+    #Content{
+        margin-top: 5%;
+    }
+    #Content #mainContent{
+        margin-left: 8%;
+    }
+    .MoreBtn{
+        margin: 5px;
+    }
+    .mainDetail{
+        line-height: 1.8em;
+        margin-top: 3%;
+    }
+</style>
