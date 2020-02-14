e35b3001dc7c710ac9ad7bde345250e3ca370599
HyeminNoh <shrmffkfk@naver.com>
2020-02-14 20:36:21 +0900

    bootstrap-vue, vue-router 추가

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
