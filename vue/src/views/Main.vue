7adb29ebd2804279285d14a2dd4a3339606ebecf
HyeminNoh <shrmffkfk@naver.com>
2020-02-14 20:34:37 +0900

    메인 뷰

---
+++ vue/src/views/Main.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <b-container>
+            <b-row>
+                <b-col cols="6">
+                    <b-embed
+                            type="iframe"
+                            aspect="16by9"
+                            src="https://www.youtube.com/embed/BVtt-_0tBa0"
+                            allowfullscreen>
+                    </b-embed>
+                </b-col>
+                <b-col>
+                    <div class="text-left">
+                        <h4>#iamnotavirus운동에 대해 아시나요?</h4>
+                        <p>
+                            신종 코로나 바이러스가 전세계에 퍼져나가면서 많은 사람들이 두려움을 느끼고 있습니다.<br>
+                            이런 두려움과 별개로 신종 코로나 바이러스가 인종 차별의 요소로 사용되고 있습니다.<br><br>
+                            모든 아시안들이 코로나를 보균했을거라는 생각은 인종 차별적인 생각입니다.<br>
+                            신종 코로나에 대해 올바르게 인식하고 모든 아시아인들에 대한 차별을 멈춰주세요.<br>
+                        </p>
+                    </div>
+                    <div class="text-right">
+                        <b-button variant="primary" href="#">코로나-19 알아보기</b-button>
+                        <b-button variant="success" href="#">전세계 현황</b-button>
+                    </div>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
