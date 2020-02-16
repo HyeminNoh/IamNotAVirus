9a807c29aa465cdaab9d2d68a7dbfbacbfc98dfb
HyeminNoh <shrmffkfk@naver.com>
2020-02-16 22:37:17 +0900

    Add News vue 관련기사크롤링 페이지

---
+++ vue/src/views/News.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="Content">
+        <b-container id="newsContent">
+            <h3>📰 인종차별 관련 기사</h3>
+            <b-row>
+                <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left class="mb-3">
+                    <b-card-text>
+                        Some quick example text to build on the card and make up the bulk of the card's content.
+                    </b-card-text>
+                </b-card>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+<style lang="css">
+    #Content{
+        margin-top: 5%;
+    }
+    #Content #newsContent{
+        margin-left: 10%;
+    }
+</style>
