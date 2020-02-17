b4a17aacdca4974e9a93bc722cb92bff27e73d6e
HyeminNoh <shrmffkfk@naver.com>
2020-02-17 23:52:57 +0900

    전세계 감염자 정보를 보여주는 화면

---
+++ vue/src/views/Infection.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="Content">
+        <b-container id="infectionContent">
+            {{statusItems}}
+        </b-container>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        name: 'status',
+        data: () => {
+            return {
+                statusItems: []
+            }
+        },
+        created () {
+            axios.get('http://happycastle.club/status')
+                .then(response => {
+                    this.statusItems = response.data
+                })
+                .catch(e => {
+                    console.log('error : ', e)
+                })
+        }
+    }
+</script>
+
+<style lang="css">
+    #Content{
+        margin-top: 5%;
+    }
+    #Content #infectionContent{
+        margin-left: 10%;
+        text-align: left;
+    }
+</style>
