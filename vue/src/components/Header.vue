0cd65798f8cbb94dee669064778897de7813144b
HyeminNoh <shrmffkfk@naver.com>
2020-02-19 18:40:59 +0900

    언어부분 한글로 수정

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -11,9 +11,9 @@
                 <!-- Right aligned nav items -->
                 <b-navbar-nav class="ml-auto">
                     <b-button variant="outline-primary" style="margin-right: 10px">응원해요✨</b-button>
-                    <b-nav-item-dropdown text="Language" right>
-                        <b-dropdown-item href="#">KR</b-dropdown-item>
-                        <b-dropdown-item href="#">EN</b-dropdown-item>
+                    <b-nav-item-dropdown text="언어" right>
+                        <b-dropdown-item href="#">한국어</b-dropdown-item>
+                        <b-dropdown-item href="#">영어</b-dropdown-item>
                     </b-nav-item-dropdown>
                 </b-navbar-nav>
             </b-navbar>
