6acd7c16a5162590058d14d632917d5dcc9ddcc0
HyeminNoh <shrmffkfk@naver.com>
2020-02-22 03:55:39 +0900

    bundleAnalyzer추가

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,18 +1,18 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    .BundleAnalyzerPlugin;
+
 module.exports = {
-    outputDir: "../src/main/resources/static",
-    indexPath: "../static/index.html",
-    devServer: {
-        proxy: {
-            "/": {
-                "target": 'http://localhost:8080/',
-                "pathRewrite": { '^/': '' },
-                "changeOrigin": true
-            }
-        }
+    outputDir: "../src/main/resources/static",
+    indexPath: "../static/index.html",
+    devServer: {
+        proxy: "http://localhost:8080"
     },
     chainWebpack: config => {
         const svgRule = config.module.rule("svg");
         svgRule.uses.clear();
         svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+    },
+    configureWebpack: {
+        plugins: [new BundleAnalyzerPlugin()]
     }
 };
