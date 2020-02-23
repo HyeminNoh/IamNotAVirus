3fd676912e1d7e30820120027fd4700078185e2c
HyeminNoh <shrmffkfk@naver.com>
2020-02-24 01:37:50 +0900

    인종차별 뉴스 내용 뷰

--- vue/src/views/News.vue
+++ vue/src/views/News.vue
@@ -1,22 +1,70 @@
 <template>
     <div id="Content">
         <b-container id="newsContent">
-            <h3>📰 인종차별 관련 기사</h3>
             <b-row>
-                <b-card img-src="https://placekitten.com/300/300" img-alt="Card image" img-left class="mb-3">
-                    <b-card-text>
-                        Some quick example text to build on the card and make up the bulk of the card's content.
-                    </b-card-text>
+                <b-col>
+                    <h3 style="text-align: left">📰 인종차별 관련 기사</h3>
+                </b-col>
+                <b-col style="text-align: right; margin-right: 2%">
+                    <b-button variant="outline-success" target="_blank" href="https://search.naver.com/search.naver?query=%EC%BD%94%EB%A1%9C%EB%82%98+%EC%9D%B8%EC%A2%85%EC%B0%A8%EB%B3%84&where=news&ie=utf8&sm=nws_hty">뉴스 더보기</b-button>
+                </b-col>
+            </b-row>
+
+            <b-row id="news-list" v-for="(item, index) in newsList"
+                   :key="index" >
+                <b-card :img-src=item.img :to=item.link target="_blank" img- img-alt="News image" img-height="120px" img-width="120px" img-left class="mb-3">
+                        <b-card-text id="news-text">
+                            <a :href=item.link target="_blank">
+                                <h4 id="news-title">{{item.title}}</h4>
+                            </a>
+                            <br>
+                            <h6>출처: {{item.source}}</h6>
+                        </b-card-text>
                 </b-card>
             </b-row>
         </b-container>
     </div>
 </template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        name: 'news',
+        data: () => {
+            return {
+                newsList: []
+            }
+        },
+        mounted () {
+            axios.get('/crawling/news/kr',
+                {headers:{'Access-Control-Allow-Origin': '*', 'Content-Type':'application/json; charset = utf-8'}})
+                .then(response => {
+                    this.newsList = response.data;
+                    console.log(response.data);
+                })
+                .catch(e => {
+                    console.log('error : ', e)
+                })
+        }
+    }
+</script>
 <style lang="css">
     #Content{
         margin-top: 5%;
     }
     #Content #newsContent{
-        margin-left: 10%;
+        margin-left: 5%;
+        text-align: center;
+    }
+    #news-text{
+        text-align: left;
+    }
+    #news-text #news-title{
+        color: black;
+    }
+    #news-text #news-title:hover{
+        outline: none;
+        color: #2c7bdc;
     }
 </style>
