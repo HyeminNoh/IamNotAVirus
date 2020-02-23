7e5f1e4968350f81a381a340443da215f25e7e0c
HyeminNoh <shrmffkfk@naver.com>
2020-02-23 23:41:37 +0900

    인종차별 관련 네이버 뉴스 크롤링 기능

--- src/main/java/com/iamnotavirus/util/WebCrawler.java
+++ src/main/java/com/iamnotavirus/util/WebCrawler.java
@@ -8,10 +8,10 @@ import java.io.IOException;
 
 public class WebCrawler {
 
-    public Object getNewsInfoKr() throws Exception{
-        Document document = new WebCrawler().getCrawlingResult("https://www.google.com/search?q=%EC%BD%94%EB%A1%9C%EB%82%98+%EC%9D%B8%EC%A2%85%EC%B0%A8%EB%B3%84&hl=ko&source=lnms&tbm=nws&sa=X&ved=2ahUKEwj8krCdnNPnAhWq-GEKHeyfClgQ_AUoAnoECAsQBA&biw=1280&bih=578");
-        Elements el = document.select("#search .bkWMgd");
-        return el.html();
+    public Elements getNewsInfoKr() throws Exception{
+        Document document = new WebCrawler().getCrawlingResult("https://search.naver.com/search.naver?where=news&query=%EC%BD%94%EB%A1%9C%EB%82%98%20%EC%9D%B8%EC%A2%85%EC%B0%A8%EB%B3%84&sm=tab_srt&sort=0&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid=&nso=so%3Ar%2Cp%3Aall%2Ca%3Aall&mynews=0&refresh_start=0&related=0");
+        Elements el = document.select("#main_pack .news.mynews.section._prs_nws .type01");
+        return el;
     }
 
     public void getNewsInfoUs() throws Exception{
