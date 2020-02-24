69004c3cd69d3b0b0268a27c2927b157d2080ff0
HyeminNoh <shrmffkfk@naver.com>
2020-02-25 02:59:33 +0900

    vue-clipboard 추가

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,13 +5,17 @@ import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueClipboard from 'vue-clipboard2'
 
 Vue.use(VueRouter);
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
-Vue.config.productionTip = false
+VueClipboard.config.autoSetContainer = true // add this line
+Vue.use(VueClipboard);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
