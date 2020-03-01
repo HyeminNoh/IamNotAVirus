d8de1a5230a26fe95963c67fd4a440e417072f78
HyeminNoh <shrmffkfk@naver.com>
2020-03-01 22:48:41 +0900

    api 링크 수정

--- vue/src/views/Infection.vue
+++ vue/src/views/Infection.vue
@@ -93,7 +93,7 @@
                 if (item.country === '중국') return 'table-danger';
             },
             getData(){
-                axios.get('/infection/data',
+                axios.get('/infectiondata',
                     {headers:{'Access-Control-Allow-Origin': '*', 'Content-Type':'application/json; charset = utf-8'}})
                     .then(response => {
                         this.statusItems = response.data;
